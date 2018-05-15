d46cbd97b77749727c6ead693c00f9fc8bc10e4d
JAG <roxxor99@users.noreply.github.com>
2018-05-14 18:47:53 -0600

    To do: Save and Retrieve data onClickItemListener CursorArray vs ArrayAdapter Notifications Cleanup

--- app/src/main/java/gunderson/wgu/org/wguapp/AlarmReceiver.java
+++ app/src/main/java/gunderson/wgu/org/wguapp/AlarmReceiver.java
@@ -1,45 +1,51 @@
 package gunderson.wgu.org.wguapp;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
 import static gunderson.wgu.org.wguapp.DatabaseHelper.TERM_START;
 
 //!!!UPDATE MANIFEST
 
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        createNotification(context);
+    }
 
-//public class AlarmReceiver extends BroadcastReceiver{
-//    @Override
-//    public void onRecieve (Context context, Intent intent){
-//        //get termStart from db & assign to variable to be compared to localdate?
-////        String courseStart = row.getString(row.getColumnIndexOrThrow(TERM_START));
-////        LocalDate ld = LocalDate.now();
-//
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-//
-//        builder.setAutoCancel(true)
-//                .setDefaults(Notification.DEFAULT_ALL)
-//
-//                //not sure what this will end being yet:
-//                    // 1- create variables for LocalDate and TERM_START and compare?
-////                .setWhen(TermModel.getTermStart == CurrentDate))
-//                .setSmallIcon(R.drawable.owl)
-//                .setContentTitle("Alert From WGU")
-//                .setContentText("Important Date Event")
-//                .setContentInfo("Please check the scheduler for an important date event.");
-//
-//        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-//        notificationManager.notify(1,builder.build());
-//
-//    }
+    private void createNotification(Context context) { //String msgTitle, String msgTicker, String msgText, String msgInfo){
 
-//}
+        //return to MainLanding
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainLanding.class), 0);
+
+        //Build the user notification
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.owl)
+//                .setTicker("Alert From WGU")
+                .setContentTitle("Alert From WGU")
+                .setContentText("Important Date Event")
+                .setContentInfo("Please check the scheduler for an important date event.")
+                .setDefaults(Notification.DEFAULT_ALL);
+
+        //the intent to run when clicked and cancel notification on click
+        notificationBuilder.setContentIntent(pendingIntent);
+        notificationBuilder.setAutoCancel(true);
+
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notificationBuilder.build());
+
+    }
+
+
+}
